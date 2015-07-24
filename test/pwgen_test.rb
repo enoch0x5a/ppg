5425383d357c5404eed66e835f044669b7a3cc45
enoch0x5a <enoch0x5a@gmail.com>
2015-07-24 12:49:23 +0300

    bug fix

--- test/pwgen_test.rb
+++ test/pwgen_test.rb
@@ -6,23 +6,29 @@ describe PwGen do
 	
 	before(:each) do
 		@pwgen = PwGen.new(8)
-		@password = @pwgen.generate_password
 	end
 
-	it "should contain 8 digit password" do
-		expect(@password.to_s.length).to eq(8)
+	describe "without mutators" do
+		before(:each) do
+			@password = @pwgen.generate_password
+		end
+		it "should contain 8 digit password" do
+			expect(@password.to_s.length).to eq(8)
+		end
+		it "should be of english chars, downcase" do 
+			expect(@password.to_s).to match(/[a-z]+/)
+		end
 	end
-	it "should be of english chars, downcase" do 
-		expect(@password.to_s).to match(/[a-z]+/)
-	end
-	it "should contain 1 digit" do
-		@pwgen.add_mutator(1, Proc.new { Random.rand(10).to_s })
-		password = @pwgen.generate_password.to_s
-		expect(password).to match(/\D*\d\D*/)
-	end
-	it "should contain 1 upper case character" do
-		@pwgen.add_mutator(1, Proc.new { |c| c.upcase })
-		password = @pwgen.generate_password.to_s
-		expect(password).to match(/[a-z]*[A-Z][a-z]*/)
+	describe "with mutators" do
+		it "should contain 1 upper case character" do
+			@pwgen.add_mutator(1, Proc.new { |c| c.upcase })
+			password = @pwgen.generate_password
+			expect(password.to_s).to match(/[a-z]*[A-Z][a-z]*/)
+		end
+		it "should contain 1 digit" do
+			@pwgen.add_mutator(1, Proc.new { Random.rand(10).to_s })
+			password = @pwgen.generate_password
+			expect(password.to_s).to match(/\D*\d\D*/)
+		end
 	end
 end
